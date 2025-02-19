708c0537ebc5e47900be647ec8f1c28fcfa4f29e
TheArtPlug <79953999+TheArtPlug@users.noreply.github.com>
2025-02-19 06:10:40 +0000

    first commit

---
+++ Welcome To [TheArtPlug] <Main> p <Datase.rb
@@ -0,0 +1,8 @@
+Welcome To [TheArtPlug] <Main> p <Dataset> 
+     <base > #html #main <ontological> 
+     # A sample Gemfile
+source "https://rubygems.org"
+
+gem 'pry' # for debugging
+gem 'pg'
+gem 'activerecord', '6.1.5.1'
